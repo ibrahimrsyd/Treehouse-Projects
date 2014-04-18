49a5cfe120c94b92eebff9526966161d20fd0e18
Elizabeth Frazier <egfraz@gmail.com>
2014-04-17 23:31:41 -0700

    Updated config.php and deleted products-new.php
    Config.php: changed BASE_URL variable from "/shirts4mike_local/" to
    "/shirts4mike" since these files will eventually live on production
    servers.
    
    Products-new.php: deleted this file since I've already transferred all
    additional data from the $products array to the original products.php
    file.

--- PHP Development Track/shirts4mike/includes/config.php
+++ PHP Development Track/shirts4mike/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 	
 	//For root relative URLs/web addresses (linking to other pages, images)
-	define("BASE_URL","/shirts4mike_local/");
+	define("BASE_URL","/shirts4mike/");
 	//For absolute paths on the server (including code from other PHP files)
-	define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/shirts4mike_local/");
+	define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/shirts4mike/");
